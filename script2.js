b1f38020909f8ae76ab06e1835eb34288d3e8912
Neethu S <neethuams@gmail.com>
2023-09-28 15:23:33 +0530

    updated file names

--- script2.js
+++ script2.js
@@ -1,14 +1,39 @@
-let tabs = document.getElementsByClassName("tab");
-for (let i = 0; i < tabs.length; i++) {
-    const tab = tabs[i];
-    tab.addEventListener("click",function(e){
-        const element = e.target;
-        const contentId = element.dataset.content;
-        let services = document.getElementsByClassName("service");
-        for (let j = 0; j < services.length; j++) {
-            services[j].classList.remove("active");
-            
-        }
-        document.getElementById(contentId).classList.add("active");
-    })
+document.getElementById("tab1").addEventListener("click",function(){
+
+    // document.getElementsByClassName("service").setAttribute("style", "display:none");
+    //document.getElementById("service1").setAttribute("style", "display:block");
+    let a = document.getElementsByClassName("service");
+    for (let i = 0; i < a.length; i++) {
+        a[i].classList.remove("active");
+        
+    }
+    document.getElementById("service1").classList.add("active");
+
+});
+document.getElementById("tab2").addEventListener("click",tab2Click);
+function tab2Click(){
+    let a = document.getElementsByClassName("service");
+    for (let i = 0; i < a.length; i++) {
+        a[i].classList.remove("active");
+        
+    }
+    document.getElementById("service2").classList.add("active");
+}
+document.getElementById("tab3").addEventListener("click",tab3Click);
+function tab3Click(){
+    let a = document.getElementsByClassName("service");
+    for (let i = 0; i < a.length; i++) {
+        a[i].classList.remove("active");
+        
+    }
+    document.getElementById("service3").classList.add("active");
+}
+document.getElementById("tab4").addEventListener("click",tab4Click);
+function tab4Click(){
+    let a = document.getElementsByClassName("service");
+    for (let i = 0; i < a.length; i++) {
+        a[i].classList.remove("active");
+        
+    }
+    document.getElementById("service4").classList.add("active");
 }
